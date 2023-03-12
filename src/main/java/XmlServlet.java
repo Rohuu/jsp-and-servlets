16d6528c603f16de95c8234775c893c89f9f648a
Rohuu <rohuu99022@gmail.com>
2023-03-12 20:31:42 +0530

    understanding GET and POST

--- src/main/java/XmlServlet.java
+++ src/main/java/XmlServlet.java
@@ -8,7 +8,10 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class XmlServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	
+	// doGet uses: when we want to actually getting something from application
+	// get method shows parameters in URL only
+	// what if too many parameters(tooLengthyURL), what if password is also a parameter(notSecure)
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
 		PrintWriter printWriter=response.getWriter();
@@ -16,6 +19,13 @@ public class XmlServlet extends HttpServlet {
 		printWriter.println("Hello "+userName+ " from GET method!");
 	}
 	
+	
+	// doPost uses: when we want to post/submit something to the server & the application has to do some kind of write operation later depending upon the data we have posted
+	// like we want to add a user to application
+	// edit or delete something
+	// when we use it, it causes application to make some changes to the data
+	// browser always warns the user that "you have made changes, if you will hit refresh it will go in previous state , just a layer of protection", if after submitting data we press reload button.
+	// suppose we made payment from our card, and again we hit reload then browser warns that data has been written so that user might reconsider doing that submission again.  
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
 		PrintWriter printWriter=response.getWriter();
