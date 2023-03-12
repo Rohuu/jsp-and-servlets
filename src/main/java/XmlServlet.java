28d29ce538fc1309faa2650d1d10420a047d1f71
Rohuu <rohuu99022@gmail.com>
2023-03-12 17:10:19 +0530

    requesting parameter using request.getParameter method

--- src/main/java/XmlServlet.java
+++ src/main/java/XmlServlet.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -9,10 +10,10 @@ public class XmlServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("XML servlet called!");
-		response.getWriter().println("XML sevlet calling is now successful");
-		// in this process we actually did the configuration not by using annotations but in the web.xml file
-		// Tomcat always looks at web.xml file when it starts up, and take note of the configuration from the web.xml file 
+		response.setContentType("text/html");
+		PrintWriter printWriter=response.getWriter();
+		String userName=request.getParameter("userName");
+		printWriter.println("Hello! "+userName);
 	}
 
 }
