9d326ac936b501a0da5dc7809e47bdb4a8f9aedb
Rohuu <rohuu99022@gmail.com>
2023-03-12 19:48:15 +0530

    passing more parameters

--- src/main/java/XmlServlet.java
+++ src/main/java/XmlServlet.java
@@ -20,6 +20,20 @@ public class XmlServlet extends HttpServlet {
 		response.setContentType("text/html");
 		PrintWriter printWriter=response.getWriter();
 		String userName=request.getParameter("userName");
+		String fullName=request.getParameter("fullName");
 		printWriter.println("Hello "+userName+" from POST method!");
+		printWriter.println("Hello "+fullName+" from POST method!");
+		String profession=request.getParameter("profession");
+		printWriter.println("You are a "+profession);
+//		String location=request.getParameter("location");
+//		printWriter.println("You are "+location);
+		// by this we can capture only a single location 
+		// but in HTML page we have written that we can select multiple locations
+		// so for that we will use below method and capture locations in a string array
+		String[] location=request.getParameterValues("location");
+		printWriter.println("You are "+location.length+" places");
+		for(int i=0;i<location.length;i++) {
+			printWriter.println(location[i]);
+		}
 	}
 }
